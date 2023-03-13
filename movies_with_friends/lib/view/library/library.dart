7cc0918f5b475142616c77d02a7ac1ec99f9c58a
diogotvf7 <93603699+diogotvf7@users.noreply.github.com>
2023-03-13 20:18:05 +0000

    Horizontal Media Slider Widget created! (#31)
    * Horizontal Media Slider Widget created!
    
    * Fixed mistake on news_widget.dart
    
    Co-authored-by: João Pereira <53405284+thePeras@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: João Pereira <53405284+thePeras@users.noreply.github.com>

--- movies_with_friends/lib/view/library/library.dart
+++ movies_with_friends/lib/view/library/library.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:movies_with_friends/view/common_widgets/poster.dart';
+import 'package:movies_with_friends/view/common_widgets/media_list.dart';
+import 'package:movies_with_friends/model/media.dart';
 
 class LibraryPage extends StatefulWidget {
   const LibraryPage({super.key});
@@ -9,6 +12,15 @@ class LibraryPage extends StatefulWidget {
 
 class _MyLibraryPageState extends State<LibraryPage> {
 
+  static const List<Media> myMedia = [
+    Media("Mad Men", "https://br.web.img3.acsta.net/pictures/21/02/10/20/02/0834301.jpg"),
+    Media("Dr. House", "https://www.themoviedb.org/t/p/original/lW7MvZ4m49IUj2UrUu4z0xVVl81.jpg"),
+    Media("Sherlock", "https://cdn.europosters.eu/image/750/posters/sherlock-destruction-i34921.jpg"),
+    Media("The Walking Dead", "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/81UvZYUoOJL.jpg"),
+    Media("Dexter", "https://cdn.europosters.eu/image/750/posters/dexter-shrinkwrapped-i14382.jpg"),
+    Media("Patrick Melrose", "https://m.media-amazon.com/images/M/MV5BMjIwNzk4OTQ1NV5BMl5BanBnXkFtZTgwMjE0NDMyNTM@._V1_.jpg")
+  ];
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -18,9 +30,10 @@ class _MyLibraryPageState extends State<LibraryPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: const <Widget>[
-            Text(
-              'Library',
-            ),
+            MediaList(
+              name: "My First Series List!",
+              media: myMedia
+            )
           ],
         ),
       ),
