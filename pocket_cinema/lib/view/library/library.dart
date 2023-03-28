a53858a0bd4d877b0e231e73abeb42c8952960f8
thePeras <jtopereira@gmail.com>
2023-03-28 23:46:31 +0100

    Create a abstract class and add the onPressed pages.
    Co-authored-by: Hmgc2002 <hmgc2016@gmail.com>
    Co-authored-by: Minipoloalex

--- pocket_cinema/lib/view/library/library.dart
+++ pocket_cinema/lib/view/library/library.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:pocket_cinema/view/common_widgets/horizontal_media_list.dart';
 import 'package:pocket_cinema/model/media.dart';
 import 'package:heroicons/heroicons.dart';
+import 'package:pocket_cinema/view/library/widgets/list_button.dart';
+import 'package:pocket_cinema/view/media_list/media_list.dart';
 
 class LibraryPage extends StatefulWidget {
   const LibraryPage({Key? key});
@@ -11,40 +13,58 @@ class LibraryPage extends StatefulWidget {
 }
 
 class _MyLibraryPageState extends State<LibraryPage> {
-  bool _isWatched = false;
-
-  static const List<Media> myMedia = [    Media("Mad Men", "https://br.web.img3.acsta.net/pictures/21/02/10/20/02/0834301.jpg"),    Media("Dr. House", "https://www.themoviedb.org/t/p/original/lW7MvZ4m49IUj2UrUu4z0xVVl81.jpg"),    Media("Sherlock", "https://cdn.europosters.eu/image/750/posters/sherlock-destruction-i34921.jpg"),    Media("The Walking Dead", "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/81UvZYUoOJL.jpg"),    Media("Dexter", "https://cdn.europosters.eu/image/750/posters/dexter-shrinkwrapped-i14382.jpg"),    Media("Patrick Melrose", "https://m.media-amazon.com/images/M/MV5BMjIwNzk4OTQ1NV5BMl5BanBnXkFtZTgwMjE0NDMyNTM@._V1_.jpg"),  ];
+  static const List<Media> myMedia = [
+    Media("Mad Men",
+        "https://br.web.img3.acsta.net/pictures/21/02/10/20/02/0834301.jpg"),
+    Media("Dr. House",
+        "https://www.themoviedb.org/t/p/original/lW7MvZ4m49IUj2UrUu4z0xVVl81.jpg"),
+    Media("Sherlock",
+        "https://cdn.europosters.eu/image/750/posters/sherlock-destruction-i34921.jpg"),
+    Media("The Walking Dead",
+        "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/81UvZYUoOJL.jpg"),
+    Media("Dexter",
+        "https://cdn.europosters.eu/image/750/posters/dexter-shrinkwrapped-i14382.jpg"),
+    Media("Patrick Melrose",
+        "https://m.media-amazon.com/images/M/MV5BMjIwNzk4OTQ1NV5BMl5BanBnXkFtZTgwMjE0NDMyNTM@._V1_.jpg"),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    final Color checkedColor = Theme.of(context).colorScheme.secondary;
-
     return Scaffold(
       body: Column(
         children: <Widget>[
-          SizedBox(height: 50),
-          HorizontalMediaList(
+          const SizedBox(height: 50),
+          const HorizontalMediaList(
             name: "My First Series List!",
             media: myMedia,
           ),
-          SizedBox(height: 50),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              ElevatedButton.icon(
+              ListButton(
+                icon: const HeroIcon(HeroIcons.checkCircle,
+                    style: HeroIconStyle.solid),
+                labelText: "Watched",
                 onPressed: () {
-                  setState(() {
-                    _isWatched = !_isWatched;
-                  });
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const MediaListPage(
+                              name: "Watched", mediaList: myMedia)));
                 },
-                icon: Icon(_isWatched ? Icons.remove_red_eye : Icons.check),
-                label: Text(_isWatched ? "Watching" : "Watched"),
-                style: ElevatedButton.styleFrom(
-                  primary: _isWatched ? Colors.grey[800] : checkedColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
+              ),
+              const SizedBox(width: 20),
+              ListButton(
+                icon: const HeroIcon(HeroIcons.ellipsisHorizontalCircle,
+                    style: HeroIconStyle.solid),
+                labelText: "Watching",
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const MediaListPage(
+                              name: "Watching", mediaList: myMedia)));
+                },
               ),
             ],
           ),
