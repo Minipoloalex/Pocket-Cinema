73df88febae8e21ed1c608197b407323f42b7d88
Félix Martins <100770338+Minipoloalex@users.noreply.github.com>
2023-04-02 09:09:09 +0100

    Firebase cli

--- pocket_cinema/lib/main.dart
+++ pocket_cinema/lib/main.dart
@@ -11,10 +11,13 @@ import 'package:pocket_cinema/view/login/login.dart';
 import 'package:pocket_cinema/view/register/register.dart';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
