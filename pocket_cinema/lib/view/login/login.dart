8e829008d73ad6f98951a436e26f6696fd81f888
Félix Martins <100770338+Minipoloalex@users.noreply.github.com>
2023-04-13 17:11:59 +0100

    Changed testing null as requested

--- pocket_cinema/lib/view/login/login.dart
+++ pocket_cinema/lib/view/login/login.dart
@@ -57,7 +57,7 @@ class _LoginPageState extends State<LoginPage> {
                   ElevatedButton(
                   onPressed: () {
                     Authentication.signInWithGoogle().then((user) {
-                      if (user == null || user.displayName == null || user.email == null) return;
+                      if (!user?.displayName || !user?.email) return;
                       Authentication.createUserGoogleSignIn(
                         MyUser(username: user.displayName, email: user.email),
                       );
