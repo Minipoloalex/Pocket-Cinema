91d4ce01d9525a6c676f862b7472c24dea8cfcc3
hmgc2002 <hmgc2016@gmail.com>
2023-04-27 22:51:47 +0100

    Forgot one import

--- pocket_cinema/lib/controller/news_provider.dart
+++ pocket_cinema/lib/controller/news_provider.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:intl/intl.dart';
 import 'package:pocket_cinema/controller/fetcher.dart';
 import 'package:pocket_cinema/model/news.dart';
 
