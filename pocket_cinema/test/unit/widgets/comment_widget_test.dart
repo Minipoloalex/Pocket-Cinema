19d9b118fcd1f16569dfa56756120ee6dc802f7a
hmgc2002 <hmgc2016@gmail.com>
2023-04-27 22:59:14 +0100

    Fixed the tests

--- pocket_cinema/test/unit/widgets/comment_widget_test.dart
+++ pocket_cinema/test/unit/widgets/comment_widget_test.dart
@@ -1,9 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:intl/intl.dart';
 import 'package:pocket_cinema/model/comment.dart';
 import 'package:pocket_cinema/view/media/widgets/comment_widget.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 import '../../testable_widget.dart';
 
@@ -20,7 +20,7 @@ void main() {
     await tester.pumpWidget(testableWidget(CommentWidget(comment: comment)));
 
     final usernameTextFinder = find.text('${comment.username}: ${comment.content}');
-    final dateTextFinder = find.text(DateFormat('hh:mm MM/dd/yyyy').format(comment.createdAt.toDate()));
+    final dateTextFinder = find.text(timeago.format(comment.createdAt.toDate()));
 
     expect(usernameTextFinder, findsOneWidget);
     expect(dateTextFinder, findsOneWidget);
