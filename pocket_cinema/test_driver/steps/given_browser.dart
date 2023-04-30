62b950400d535ce21bf2bada101159ebc3c5be08
diogotvf7 <up202108752@g.uporto.pt>
2023-04-30 12:13:11 +0100

    Optimized imports

--- pocket_cinema/test_driver/steps/given_browser.dart
+++ pocket_cinema/test_driver/steps/given_browser.dart
@@ -1,3 +1,1 @@
-import 'package:flutter_gherkin/flutter_gherkin.dart';
-import 'package:flutter_driver/flutter_driver.dart';
-import 'package:gherkin/gherkin.dart';
+
--- pocket_cinema/test_driver/steps/when_tap_name_media.dart
+++ pocket_cinema/test_driver/steps/when_tap_name_media.dart
@@ -1,6 +1,6 @@
+import 'package:flutter_driver/flutter_driver.dart';
 import 'package:flutter_gherkin/flutter_gherkin.dart';
 import 'package:gherkin/gherkin.dart';
-import 'package:flutter_driver/flutter_driver.dart';
 
 class WhenTapNameMedia extends When2WithWorld<String, String, FlutterWorld> {
   @override
