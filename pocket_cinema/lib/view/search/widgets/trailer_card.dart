d52df6d9e58d344ef30ae835ed5bb98c6ba46778
thePeras <jtopereira@gmail.com>
2023-05-01 12:30:13 +0100

    Set controls in player controller and play the trailers

--- pocket_cinema/lib/view/search/widgets/trailer_card.dart
+++ pocket_cinema/lib/view/search/widgets/trailer_card.dart
@@ -3,7 +3,7 @@ import 'package:heroicons/heroicons.dart';
 import 'package:pocket_cinema/controller/fetcher.dart';
 import 'package:pocket_cinema/controller/parser.dart';
 import 'package:pocket_cinema/model/media.dart';
-import 'package:pocket_cinema/view/common_widgets/video_player.dart';
+import 'package:pocket_cinema/view/common_widgets/trailer_player.dart';
 import 'package:pocket_cinema/view/media/media_page.dart';
 
 class TrailerCard extends StatelessWidget {
@@ -75,11 +75,6 @@ class TrailerCard extends StatelessWidget {
                 icon:
                     const HeroIcon(HeroIcons.play, style: HeroIconStyle.solid),
                 onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const TrailerPlayer()));
-
                   if (media.trailer == null) {
                     ScaffoldMessenger.of(context).showSnackBar(
                         const SnackBar(content: Text("No trailer available")));
@@ -90,7 +85,17 @@ class TrailerCard extends StatelessWidget {
                       .then((playbacksResponse) {
                     final List playbacks =
                         Parser.movieTrailerPlaybacks(playbacksResponse);
-                    print(playbacks);
+                    if (playbacks.isEmpty) {
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                          content: Text("No trailer available")));
+                      return;
+                    }
+
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => TrailerPlayer(
+                                videoUrl: playbacks[0]['url'])));
                   });
                 },
                 style: ButtonStyle(
